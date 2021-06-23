b85027ecd35beed3187efa0c00079d3a46f40b65
huanwu <zhaopanhw@foxmail.com>
2021-06-23 21:43:18 +0800

    chore:整理注释

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,8 @@ import Loadable from '@loadable/component' // 用于代码分割时动态加载�
 import { RouteConfig } from 'react-router-config'
 
 /**
- * @exact 是否采用精确匹配,嵌套路由不要加exact属性，如果父级路由加上，这里例如topics加上该属性，他下面的子路由将不会生效，因为外层强制匹配了。
+ * @exact 是否采用精确匹配,嵌套路由不要加exact属性，如果父级路由加上，
+ * 这里例如layout加上该属性，他下面的子路由将不会生效，因为外层强制匹配了。
  */
 const routesConfig: RouteConfig[] = [
     {
